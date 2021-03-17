7a084eb84a7be3cacab3d2fcad2384bc1b760db2
Oleg Marchenko <omarchenko.150@gmail.com>
2021-03-17 21:32:53 +0200

    Add a repository count method implementation generator

--- graphine-processor/src/main/java/io/graphine/processor/code/renderer/parameter/ParameterIndexProvider.java
+++ graphine-processor/src/main/java/io/graphine/processor/code/renderer/parameter/ParameterIndexProvider.java
@@ -3,6 +3,7 @@ package io.graphine.processor.code.renderer.parameter;
 /**
  * @author Oleg Marchenko
  */
+@FunctionalInterface
 public interface ParameterIndexProvider {
     String getParameterIndex();
 }
